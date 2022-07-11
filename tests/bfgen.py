cc835e6b17ab109e5350dbfd5903ac44b4707ebc
Luis Fernando Estrozi <lestrozi@gmail.com>
2022-07-11 22:22:30 +0100

    pong ball moving handling overflow and boundaries + computer paddle moving

--- tests/bfgen.py
+++ tests/bfgen.py
@@ -10,7 +10,7 @@ class BfGen():
 
         print()
 
-        self.curPos = len(generators) - 6   # FIXME
+        self.curPos = len(generators) - 7   # FIXME
 
     def moveToPos(self, goalPos):
         d = self.curPos - goalPos
@@ -53,18 +53,20 @@ generators = [
         ('paddle_left_y', ">>+>+>+[>+[-<++++>]<<]>[-<+<+>>]"),      # 105
         ('paddle_right_y', ""),                                     # 105
         ('ball_x0', ""),                                            # 0
-        ('ball_x1', ">+>+>+>+[>+[-<+++>]<<]>>"),                    # 160
+        ('ball_x1', ">+>+>+++[+>+[-<++++>]<<]>>"),                 # 158
         ('ball_y0', ">"),                                           # 0
         ('ball_y1', ">+++++++++[<+++++++++++++>-]"),                # 117
         ('ff', ">+>+>+>+[++>[-<++++>]<<]>[-<+>]"),                  # 255
         ('device', "++++[>++++<-]>[<++++++++>-]<+>"),               # 129
         ('f0', ">+>+>+>+[++>+[-<+++>]<<]>[-<+>]"),                  # 240
         ('ball_direction_x', ""),                                   # 0 (left)
-        ('ball_direction_y', ""),                                   # 0 (top)
+        ('ball_direction_y', ">"),                                  # 0 (top)
         ('mem0', ""),                                               # 0
         ('mem1', ""),                                               # 0
         ('mem2', ""),                                               # 0
         ('mem3', ""),                                               # 0
+        ('mem4', ""),                                               # 0
+        ('mem5', ""),                                               # 0
 ]
 
 bfgen = BfGen(generators)
@@ -103,12 +105,12 @@ bfgen.output("[")   # main loop [
 bfgen.output("05.ball_x0.ball_x1.ball_y0.ball_y1.00.05.00.05.00.00.00.")
 
 # moves ball_x, handling over/underflow
-bfgen.output("ball_direction_x[-mem0+mem1+ball_direction_x]mem1[-ball_direction_x+mem1]+ball_x1-----[-mem0+ball_x1]mem0[mem1-]mem2")
+bfgen.output("ball_direction_x[-mem0+mem1+ball_direction_x]mem1[-ball_direction_x+mem1]+ball_x1-----[-mem0+ball_x1]mem0+++[mem1-]mem2")
 bfgen.curPos = bfgen.labels["mem1"]
-bfgen.output("[ball_y0+ball_x0[-ball_y0-ball_x0]ball_y0[-ball_x0+ball_y0]ball_x1+ball_x0[mem1+++++++[-mem0++++++++mem1]+mem0+ball_x1-]ball_y0")
+bfgen.output("[ball_y0+ball_x0[-ball_y0-ball_x0]ball_y0[-ball_x0+ball_y0]ball_x1+ball_x0[mem0+++++ball_x1-]ball_y0")
 bfgen.curPos = bfgen.labels["ball_x1"]
-bfgen.output("[mem0------ball_x1-ball_y0]mem1-mem2]")
-bfgen.output("mem0[-ball_x1+mem0]")
+bfgen.output("[mem0-----ball_x1-ball_y0]mem1-mem2]")
+bfgen.output("mem0---[-ball_x1+mem0]")
 
 bfgen.output("ball_direction_y[-mem0+ball_y1+ball_direction_y]mem0[-ball_direction_y+mem0]ball_y1-----")
 
@@ -124,7 +126,59 @@ bfgen.output("mem2+++++++++++++++++++[mem1-mem2[-]]")
 bfgen.output("mem1[-ball_direction_y----------mem1]")
 bfgen.output("mem0[-ball_y1+mem0]")
 
+# check ball boundaries
+# mem0 = ball_x0-ball_x1, using ball_y0 as a buffer
+print("start boundaries check")
+bfgen.output("ball_x0[-ball_y0+mem1+ball_x0]ball_y0[-ball_x0+ball_y0]")
+bfgen.output("ball_x1[-ball_y0+mem0+ball_x1]ball_y0[-ball_x1+ball_y0]")
+bfgen.output("mem1[-mem0-mem1]")
+bfgen.output("mem0[-mem2+mem3+mem0]mem2[-mem0+mem2]")
+# if mem0 == 13, reached left boundary
+bfgen.output("mem1+mem0-------------[mem1-]mem2")
+bfgen.curPos = bfgen.labels["mem1"]
+bfgen.output("[ball_direction_x++++++++++mem1-mem2]mem0[-]")
+
+# now repeat the check for mem0 = 49 (reached right boundary)
+bfgen.output("mem3[-mem0+mem3]")
+bfgen.output("mem1+mem0----------------------------------------------[mem1-]mem2")
+bfgen.curPos = bfgen.labels["mem1"]
+bfgen.output("[ball_direction_x----------mem1-mem2]mem0[-]")
+bfgen.output("mem1[-]mem2[-]mem3[-]")
+print("end boundaries check")
+
+# draw new ball
 bfgen.output("05.ball_x0.ball_x1.ball_y0.ball_y1.00.05.00.05.f0.f0.00.")
+
+
+print("move right paddle (computer)")
+# move paddle_right_x
+bfgen.output("mem0+ball_direction_x[")  # if ball_direction_x != 0
+
+bfgen.output("05.01.paddle_right_x.00.paddle_right_y.00.05.00.paddle_size.00.00.00.")
+
+# use ball_y0 as buffer to copy ball_y1 to mem2 and paddle_right_y to mem3
+bfgen.output("ball_y1[-ball_y0+mem2+ball_y1]ball_y0[-ball_y1+ball_y0]")
+bfgen.output("paddle_right_y[-ball_y0+mem3+paddle_right_y]ball_y0[-paddle_right_y+ball_y0]")
+# compare mem2 and mem3
+bfgen.output("mem5+mem3+++++++++++++++mem2[-mem3-[mem4]mem2]mem5")
+bfgen.curPos = bfgen.labels["mem4"]
+bfgen.output("[-")
+# mem2 >= mem3
+bfgen.output("paddle_left_y-----mem3")  # must be 1 position before paddle_right_x
+bfgen.curPos = bfgen.labels["mem4"]
+
+bfgen.output("]mem5")
+
+# mem2 < mem3
+bfgen.output("[-")
+bfgen.output("paddle_right_y+++++mem5")
+
+bfgen.output("]mem3[-]mem2[-]")
+
+bfgen.output("05.01.paddle_right_x.00.paddle_right_y.00.05.00.paddle_size.f0.f0.f0.")
+bfgen.output("mem0-ball_direction_x[mem1+ball_direction_x-]")
+bfgen.output("]mem1[ball_direction_x+mem1-]mem0[-]")
+print("BBB")
 
 # return to initial position of the loop
 bfgen.output("05")
